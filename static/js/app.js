a399f7d8bbfab6fa8e80a3a70a1758a89a802642
Dan Rapp <Danvrapp@windstream.net>
2019-05-28 12:06:31 -0400

    Added comments to code

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,5 @@
+// Define variables for label
+
 var age;
 var bType = "";
 var ethnicity = "";
@@ -6,6 +8,7 @@ var loc = "";
 var wFreq = "";
 var samNum;
 
+//Create array of dictionaries to ID data
 var dataArray = [{
   Header:"Age:",
   Value:""
@@ -41,6 +44,7 @@ function buildMetadata(sample) {
     // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
   d3.json(`metadata/${sample}`).then(function(data) {
+    //Store metadata into appropriate dictionary
     dataArray[0].Value = data.AGE;
     dataArray[1].Value = data.BBTYPE;
     dataArray[2].Value = data.ETHNICITY;
@@ -50,6 +54,7 @@ function buildMetadata(sample) {
     dataArray[6].Value = data.sample;
     console.log("dataArray:", dataArray);
 
+    //Build lable on HTML page
     d3.select("tbody")
       .html("")
       .selectAll("td")
@@ -60,25 +65,17 @@ function buildMetadata(sample) {
         return `<td>${d.Header}</td><td>${d.Value}</td>`;
     })
   });
-
-    // Use `.html("") to clear any existing metadata
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
 }
 
 function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
   var URL = "/samples";
   d3.json(`${URL}/${sample}`).then(function(data) {
+     // Get top 10 bacteria from the sample
      var topTenIDs = data.otu_ids.slice(0, 10);
      var topTenLabels = data.otu_labels.slice(0, 10);
      var topTenValues = data.sample_values.slice(0, 10);
+     //Set up type of chart
      var trace1 = {
       labels: topTenIDs,
       values: topTenValues,
@@ -91,15 +88,16 @@ function buildCharts(sample) {
     var layout = {
       title: "% of 10 most present bacteria type",
     };
-
+    // Create pie chart
     Plotly.newPlot("pie", data, layout);
   });
-    // @TODO: Build a Bubble Chart using the sample data
+
     d3.json(`${URL}/${sample}`).then(function(data) {
       const IDs = data.otu_ids;
       const Labels = data.otu_labels;
       const Values = data.sample_values;
- 
+
+      // Set up bubble chart
       var trace1 = {
        x: IDs,
        y: Values,
